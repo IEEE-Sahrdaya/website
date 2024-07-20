8292fdcb54edd53fb471e574acd18d995464a3bb
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-07-20 08:05:49 +0530

    chore: Remove unnecessary code from Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+"use strict";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h2 className="text-xl font-bold text-center">Home Page</h2>
+      <h2 className="text-xl font-bold text-center"></h2>
     </div>
   );
 };
