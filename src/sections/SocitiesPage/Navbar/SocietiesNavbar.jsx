8da1414d69448c893014d7f1bdf39806775d7faa
Vivek K J <vivekkj2004@gmail.com>
2024-08-18 22:14:10 +0530

    fix: make society navbar responsive

--- src/sections/SocitiesPage/Navbar/SocietiesNavbar.jsx
+++ src/sections/SocitiesPage/Navbar/SocietiesNavbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 
 const SocietiesNavbar = ({ logo, navLinks, buttonLink }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Nav>
+      <div className="flex md:w-1/2 w-full items-center justify-between">
+
       <Logo>
         <Image
           src={logo.src}
@@ -12,19 +20,24 @@ const SocietiesNavbar = ({ logo, navLinks, buttonLink }) => {
           width={logo.width}
           height={logo.height}
           layout="intrinsic"
-        />
+          />
       </Logo>
 
-      <div className="flex gap-3 items-center ">
-        <NavLinks>
-          {navLinks.map((link, index) => (
-            <NavItem key={index} href={link.href}>
-              {link.text}
-            </NavItem>
-          ))}
-        </NavLinks>{" "}
+      <MenuToggle onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </MenuToggle>
+          </div>
+
+      <NavLinks className={isOpen ? "active" : ""}>
+        {navLinks.map((link, index) => (
+          <NavItem key={index} href={link.href}>
+            {link.text}
+          </NavItem>
+        ))}
         <JoinButton href={buttonLink.href}>{buttonLink.text}</JoinButton>
-      </div>
+      </NavLinks>
     </Nav>
   );
 };
@@ -35,16 +48,49 @@ const Nav = styled.nav`
   align-items: center;
   padding: 1rem 2rem;
   background-color: #fff;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const Logo = styled.div`
   flex-shrink: 0;
 `;
 
+const MenuToggle = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+
+  span {
+    height: 2px;
+    width: 25px;
+    background: #0070f3;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
 const NavLinks = styled.div`
   display: flex;
   gap: 2rem;
-  flex-origin: end;
+  align-items: center;
+  @media (max-width: 768px) {
+    display: none;
+    flex-direction: column;
+    width: 100%;
+    gap: 1rem;
+
+    &.active {
+      display: flex;
+    }
+  }
 `;
 
 const NavItem = styled.a`
@@ -74,6 +120,11 @@ const JoinButton = styled.a`
   &:hover {
     background-color: #005bb5;
     transform: scale(1.05);
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    text-align: center;
   }
 `;
 
